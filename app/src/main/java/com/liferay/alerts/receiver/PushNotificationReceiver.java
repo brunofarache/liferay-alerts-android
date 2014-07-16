b5d844d99bf7f9d455f00c85410181b2f3c5e8ea
Bruno Farache <bruno.farache@liferay.com>
2014-07-16 20:51:18 -0300

    Receive notification and display it

--- app/src/main/java/com/liferay/alerts/receiver/PushNotificationReceiver.java
+++ app/src/main/java/com/liferay/alerts/receiver/PushNotificationReceiver.java
@@ -14,10 +14,15 @@
 
 package com.liferay.alerts.receiver;
 
+import android.app.Activity;
+
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 
 import android.support.v4.content.WakefulBroadcastReceiver;
+
+import com.liferay.alerts.service.PushNotificationService;
 
 /**
  * @author Bruno Farache
@@ -26,6 +31,13 @@ public class PushNotificationReceiver extends WakefulBroadcastReceiver {
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
+		ComponentName component = new ComponentName(
+			context.getPackageName(), PushNotificationService.class.getName());
+
+		intent.setComponent(component);
+
+		startWakefulService(context, intent);
+		setResultCode(Activity.RESULT_OK);
 	}
 
 }
--- app/src/main/java/com/liferay/alerts/service/PushNotificationService.java
+++ app/src/main/java/com/liferay/alerts/service/PushNotificationService.java
@@ -15,8 +15,22 @@
 package com.liferay.alerts.service;
 
 import android.app.IntentService;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 
+import android.content.Context;
 import android.content.Intent;
+
+import android.os.Bundle;
+
+import android.support.v4.app.NotificationCompat.BigTextStyle;
+import android.support.v4.app.NotificationCompat.Builder;
+
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+
+import com.liferay.alerts.R;
+import com.liferay.alerts.activity.SignInActivity;
+import com.liferay.alerts.receiver.PushNotificationReceiver;
 
 /**
  * @author Bruno Farache
@@ -29,6 +43,38 @@ public class PushNotificationService extends IntentService {
 
 	@Override
 	protected void onHandleIntent(Intent intent) {
+		GoogleCloudMessaging gcm = GoogleCloudMessaging.getInstance(this);
+		String type = gcm.getMessageType(intent);
+		Bundle extras = intent.getExtras();
+
+		if (GoogleCloudMessaging.MESSAGE_TYPE_MESSAGE.equals(type) &&
+			!extras.isEmpty()) {
+
+			String message = extras.getString("data");
+			_showNotification(message);
+		}
+
+		PushNotificationReceiver.completeWakefulIntent(intent);
 	}
+
+	private void _showNotification(String message) {
+		NotificationManager manager = (NotificationManager)getSystemService(
+			Context.NOTIFICATION_SERVICE);
+
+		PendingIntent intent = PendingIntent.getActivity(
+			this, 0, new Intent(this, SignInActivity.class), 0);
+
+		Builder builder = new Builder(this);
+
+		builder.setSmallIcon(R.drawable.ic_launcher);
+		builder.setContentTitle(getString(R.string.app_name));
+		builder.setStyle(new BigTextStyle().bigText(message));
+		builder.setContentText(message);
+		builder.setContentIntent(intent);
+
+		manager.notify(_NOTIFICATION_ID, builder.build());
+	}
+
+	private static final int _NOTIFICATION_ID = 1;
 
 }
