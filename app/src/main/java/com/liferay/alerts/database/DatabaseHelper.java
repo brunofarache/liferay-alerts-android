84d5148d48ba5e33b582cdab1936f3d340f4e726
Bruno Farache <bruno.farache@liferay.com>
2014-09-18 06:20:01 -0300

    rename message column to payload and update values with serialized json

--- app/src/main/java/com/liferay/alerts/database/DatabaseHelper.java
+++ app/src/main/java/com/liferay/alerts/database/DatabaseHelper.java
@@ -16,8 +16,14 @@ package com.liferay.alerts.database;
 
 import android.content.Context;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import com.liferay.alerts.model.Alert;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * @author Silvio Santos
@@ -63,15 +69,72 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 	@Override
 	public void onUpgrade(
 		SQLiteDatabase database, int oldVersion, int newVersion) {
+
+		if (newVersion == 2) {
+			_upgradeToVersion2(database);
+		}
 	}
 
 	private DatabaseHelper(Context context) {
 		super(context, _DATABASE_NAME, null, _DATABASE_VERSION);
 	}
 
+	private void _upgradeToVersion2(SQLiteDatabase database) {
+		StringBuilder alterTable = new StringBuilder();
+
+		alterTable.append("ALTER TABLE ");
+		alterTable.append(AlertDAO.TABLE_NAME);
+		alterTable.append(" RENAME COLUMN ");
+		alterTable.append(Alert.MESSAGE);
+		alterTable.append(" TO ");
+		alterTable.append(Alert.PAYLOAD);
+
+		database.execSQL(alterTable.toString());
+
+		StringBuilder select = new StringBuilder();
+
+		select.append("SELECT ");
+		select.append(Alert.ID);
+		select.append(", ");
+		select.append(Alert.PAYLOAD);
+		select.append(" FROM");
+		select.append(AlertDAO.TABLE_NAME);
+
+		Cursor cursor = database.rawQuery(select.toString(), null);
+
+		while (cursor.moveToNext()) {
+			try {
+				long id = cursor.getLong(cursor.getColumnIndex(Alert.ID));
+				String payload = cursor.getString(
+					cursor.getColumnIndex(Alert.PAYLOAD));
+
+				JSONObject jsonObject = new JSONObject();
+				jsonObject.put(Alert.MESSAGE, payload);
+
+				StringBuilder update = new StringBuilder();
+
+				update.append("UPDATE ");
+				update.append(AlertDAO.TABLE_NAME);
+				update.append(" SET ");
+				update.append(Alert.PAYLOAD);
+				update.append(" = '");
+				update.append(jsonObject.toString());
+				update.append("' WHERE ");
+				update.append(Alert.ID);
+				update.append(" = ");
+				update.append(id);
+
+				database.execSQL(update.toString());
+			}
+			catch (JSONException je) {
+				je.printStackTrace();
+			}
+		}
+	}
+
 	private static final String _DATABASE_NAME = "liferay-alerts.db";
 
-	private static final int _DATABASE_VERSION = 1;
+	private static final int _DATABASE_VERSION = 2;
 
 	private static final String _FOREIGN_KEYS_ON = "PRAGMA foreign_keys = ON;";
 
